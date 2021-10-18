fd09c079888e30587351c583760903ec1cd8e86c
Maxim T <proxy9@yandex.ru>
2021-10-18 10:43:27 +0500

    Remake Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int clientsAccount = 300;
-        int deposit = 2000;
+        int deposit = 20_000;
         int bonusGift = 1;
         int gift = 100;
 
@@ -10,12 +10,13 @@ public class Main {
 
         if (deposit >= 1000) {
             bonus = deposit / gift * bonusGift;
-            total = clientsAccount + bonus + deposit;
-        }
+                    }
         else {
             bonus = 0;
-            total = clientsAccount + deposit;
-        }
+            }
+
+        total = clientsAccount + bonus + deposit;
+
         System.out.println("Количество бонусных рублей = " + bonus);
         System.out.println("Итоговый счет клиента = " + total);
     }
